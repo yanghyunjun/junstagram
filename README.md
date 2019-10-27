01c98239255bd7dfb02de328afb7d0533f1a6189
yanghyunjun <akakak231@gmail.com>
2019-10-27 23:45:18 +0900

    Create requestSecret and confirmSecret to use JWT

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # junstagram
 
 Instagram clone coding with Express + Prisma + React and React native
+
+# confirmSecret about login code
+
+Use JWT to return the login secret code.
--- src/utils.js
+++ src/utils.js
@@ -5,6 +5,7 @@ dotenv.config({ path: path.resolve(__dirname, ".env") });
 import { adjactives, nouns } from "./words";
 import nodemailer from "nodemailer";
 import sgTransport from "nodemailer-sendgrid-transport";
+import jwt from "jsonwebtoken";
 
 export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjactives.length);
@@ -27,7 +28,9 @@ export const sendSecretMail = (address, secret) => {
     from: "akakak231@gmail.com",
     to: address,
     subject: "Login Secret for Junstagram",
-    html: `Hello! Your login secret it ${secret}.</br>Copy paste on th app/web to log in`
+    html: `Hello! Your login secret it <strong>${secret}</strong>,<p>Copy paste on th app/web to log in</p>`
   };
   return sendMail(email);
 };
+
+export const generateToken = id => jwt.sign({ id }, process.env.JWT_KEY);
