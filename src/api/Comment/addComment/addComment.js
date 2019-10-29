1b382973e1d9553d31f56399d6a929deb0118c6a
yanghyunjun <akakak231@gmail.com>
2019-10-29 14:45:22 +0900

    Create to addComment function

---
+++ src/api/Comment/addComment/addComment.js
@@ -0,0 +1,26 @@
+import { isAutenticated } from "../../../middlewares";
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+  Mutation: {
+    addComment: async (_, args, { request }) => {
+      isAutenticated(request);
+      const { text, postId } = args;
+      const { user } = request;
+      const comment = await prisma.createComment({
+        user: {
+          connect: {
+            id: user.id
+          }
+        },
+        post: {
+          connect: {
+            id: postId
+          }
+        },
+        text
+      });
+      return comment;
+    }
+  }
+};
